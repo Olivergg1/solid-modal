9b065938a790949f41e427a2f05a6e18de93f223
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2022-06-10 10:05:25 +0200

    updated new types in examples

--- examples/basic/src/App.jsx
+++ examples/basic/src/App.jsx
@@ -1,14 +1,11 @@
 import styles from './App.module.css'
-import { createModal } from '../../../lib/modal'
+import { createInformationModal } from '../../../lib/modal'
 
 function App() {
-  const [show, close] = createModal({
+  const [show, close] = createInformationModal({
     title: 'Example Modal',
-    element: (
-      <button onClick={() => close()} className='button stretch'>
-        close
-      </button>
-    ),
+    text: 'This is an informational modal.',
+    elements: '',
   })
 
   return (
